c2f1af9339904e5f2b7f7f715113e178a66f89d5
huangbo.song <huangbo.song@phicomm.com>
2018-07-21 12:48:36 +0800

    add CustomUser
    Change-Id: Id7b6f38c5c9f827597a2d21e14ef64976c3223a9

--- eSchool/eSchool/settings.py
+++ eSchool/eSchool/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'pages',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- eSchool/users/models.py
+++ eSchool/users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # age = models.PositiveIntegerField(default=10)
+    nickname = models.CharField(max_length=22,default='nickname')
+    realname = models.CharField(max_length=22,default='realname')
+    birth = models.DateField(null=True,blank=True)
+    interest = models.TextField(null=True,blank=True)
+
+    VISITOR = 0
+    STUDENT = 1
+    TEACHER = 2
+    PARENT = 3
+    ROLES = (
+        (VISITOR, 'Visitor'),
+        (STUDENT,
+         'Student'),
+        (TEACHER,
+         'Teacher'),
+        (PARENT, 'Parent'),
+    )
+    userroles = models.IntegerField(
+        choices=ROLES,
+        default=VISITOR)
+
+
