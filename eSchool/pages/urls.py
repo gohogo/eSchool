8c293094bf8d548433ef354d3d0e33f2501e1e02
huangbo.song <huangbo.song@phicomm.com>
2018-07-22 20:22:23 +0800

    添加整体几个页面
    Change-Id: I192e221500f8065d23be777570f986f69b41641d

--- eSchool/pages/urls.py
+++ eSchool/pages/urls.py
@@ -6,21 +6,12 @@ from . import views
 urlpatterns = [
     # previous login view
     path('home/', views.home_page, name='homepage'),
+    path('about/',views.about_page,name='aboutpage'),
+    path('service/',views.service_page,name='servicepage'),
+    path('project/',views.project_page,name='projectpage'),
+    path('course/',views.course_page,name='coursepage'),
+    path('contact/',views.contact_page,name='contactpage'),
+    path('location/',views.location_page,name='locationpage'),
     path('hometest/',views.home_test,name='hometest'),
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('', views.dashboard, name='dashboard'),
-    # # # change password urls
-    # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    # # reset password urls
-    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    # # alternative way to include authentication views
-    # # path('', include('django.contrib.auth.urls')),
-    # path('register/', views.register, name='register'),
-    # path('edit/', views.edit, name='edit'),
-    # path('go/',views.test,name='test'),
+    path('',views.home_page, name='homepage'),
 ]
--- eSchool/pages/views.py
+++ eSchool/pages/views.py
@@ -2,7 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-	return render(request,'index.html')
-	
+	return render(request,'homepage.html')
+
+def about_page(request):
+	return render(request,'aboutpage.html')	
+
+def service_page(request):
+	return render(request,'servicepage.html')	
+
+def project_page(request):
+	return render(request,'projectpage.html')
+
+def course_page(request):
+	return render(request,'coursepage.html')
+
+def contact_page(request):
+	return render(request,'contactpage.html')
+
+def location_page(request):
+	return render(request,'baidumap.html')
+
 def home_test(request):
 	return render(request,'hometest.html')
